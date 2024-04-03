8a01ba4b2788c51db684adcd6469fe909ec8d663
Valerie Ellis <valerie.ellis@flatironschool.com>
2024-04-02 22:14:09 -0400

    try recommit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+return Math.abs(someValue - 42)
+}
+
+function distanceFromHqInFeet(someValue){
+    let someOtherValue=distanceFromHqInBlocks(someValue)
+    return someOtherValue* 264
+}
+
+
+function distanceTravelledInFeet(start, destination){
+return Math.abs(destination - start) * 264
+}
+
+function calculatesFarePrice(start, destination){
+let dtif=distanceTravelledInFeet(start, destination)
+if(dtif <= 400){
+    return 0
+  } else if(dtif > 400 && dtif < 2000){
+      return (dtif-400)*0.02
+    } else if(dtif > 2000 && dtif < 2500){
+      return 25
+    } else {
+      return 'cannot travel that far'
+}}
